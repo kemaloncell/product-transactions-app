0c0c25bd936ed9fa22ddfa0937e71e6e3b606ec0
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-12-26 23:50:50 +0300

    Configuring links with <router-link>

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,14 +1,15 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #563F7A">
-    <a class="navbar-brand" href="#">Product List</a>
+    <router-link class="navbar-brand" tag="a" to="/">Product List</router-link>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Product Transactions</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Product Output</a>
-        </li>
+        <router-link active-class="active" class="nav-link" tag="li" to="/product-transactions">
+          <a class="nav-link">Product Transactions</a>
+        </router-link>
+
+        <router-link active-class="active" class="nav-link" tag="li" to="/product-output">
+          <a class="nav-link">Product Output</a>
+        </router-link>
       </ul>
     </div>
   </nav>

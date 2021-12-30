e99190149e6409077148ef1854a4064ead9519d2
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-12-30 20:35:00 +0300

    Updating data on footer

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -3,15 +3,22 @@
     <span class="navbar-text mr-auto"> <strong>Kemal Öncel</strong> </span>
     <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
       <strong>Purchase Amount : </strong>
-      <span class="badge badge-danger">34,56</span>
+      <span class="badge badge-danger">{{ getTraderesult.purchase }}</span>
     </span>
     <span class="navbar-text navbar-nav my-2 my-lg-0 mr-3">
       <strong>Sales Amount : </strong>
-      <span class="badge badge-success">34,56</span>
+      <span class="badge badge-success">{{ getTraderesult.sale }}</span>
     </span>
     <span class="navbar-text navbar-nav my-2 my-lg-0">
       <strong>Balance : </strong>
-      <span class="badge badge-primary">34,56</span>
+      <span class="badge badge-primary">{{ getTraderesult.balance }}</span>
     </span>
   </nav>
 </template>
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: mapGetters(["getTraderesult"]),
+};
+</script>

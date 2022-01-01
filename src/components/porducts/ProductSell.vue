efbb7c8c16f9cd7ce8e47522a6d3d5b3fcd0a84a
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2022-01-01 18:35:16 +0300

    Displaying the product list in the product checkout section

--- src/components/porducts/ProductSell.vue
+++ src/components/porducts/ProductSell.vue
@@ -7,12 +7,8 @@
           <hr />
           <div class="form-group">
             <label>Product Name</label>
-            <select class="form-control">
-              <option value="1">Product 1</option>
-              <option value="1">Product 2</option>
-              <option value="1">Product 3</option>
-              <option value="1">Product 4</option>
-              <option value="1">Product 5</option>
+            <select class="form-control" v-model="selectedProduct">
+              <option :value="products.key" v-for="products in getProducts" :key="products.key">{{ products.title }}</option>
             </select>
           </div>
           <div class="card mb-2 border border-danger">
@@ -20,8 +16,8 @@
               <div class="row">
                 <div class="col-12 text-center">
                   <div class="mb-3">
-                    <span class="badge badge-info">Stok : 4</span>
-                    <span class="badge badge-primary">Price : 100,5 TL</span>
+                    <span class="badge badge-info">Stok : {{ getProducts.count }}</span>
+                    <span class="badge badge-primary">Price : {{ getProducts.price | currency }}</span>
                   </div>
                   <p class="border border-warning p-2 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda debitis deleniti eos impedit iste numquam quos sit. Dignissimos, mollitia nemo officia reiciendis repellendus rerum velit. Eos libero magnam quas tempore!</p>
                 </div>
@@ -41,7 +37,15 @@
 </template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      selectedProduct: null,
+    };
+  },
+  computed: mapGetters(["getProducts"]),
+};
 </script>
 
 <style scoped>
